b4d8e9f2775d9dc7227f37f48c0e94fb7f2fd28a
jcruz375 <joaocruz375@gmail.com>
2022-01-15 02:16:15 -0300

    change menu

--- src/components/Sidebar/SidebarNav.tsx
+++ src/components/Sidebar/SidebarNav.tsx
@@ -1,4 +1,4 @@
-import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
+import { LaptopOutlined, UserOutlined } from '@ant-design/icons';
 import { Menu } from 'antd';
 
 const { SubMenu } = Menu;
@@ -11,21 +11,12 @@ export function SidebarNav() {
       defaultOpenKeys={['sub1']}
       style={{ height: '100%', borderRight: 0 }}
     >
+      <SubMenu key="sub2" icon={<LaptopOutlined />} title="Dashboard" />
       <SubMenu key="sub1" icon={<UserOutlined />} title="Clientes">
         <Menu.Item key="1">Incluir</Menu.Item>
         <Menu.Item key="2">Alterar</Menu.Item>
         <Menu.Item key="3">Excluir</Menu.Item>
         <Menu.Item key="4">Visualizar</Menu.Item>
-      </SubMenu>
-      <SubMenu key="sub2" icon={<LaptopOutlined />} title="Configurações">
-        <Menu.Item key="5">option5</Menu.Item>
-        <Menu.Item key="6">option6</Menu.Item>
-        <Menu.Item key="7">option7</Menu.Item>
-        <Menu.Item key="8">option8</Menu.Item>
-      </SubMenu>
-      <SubMenu key="sub3" icon={<NotificationOutlined />} title="subnav 3">
-        <Menu.Item key="9">option9</Menu.Item>
-        <Menu.Item key="10">option10</Menu.Item>
       </SubMenu>
     </Menu>
   )
